0ab42ab2af8e5a2df2cd5892a83736b891c25e77
jpgfour <jeremy.grise@gmail.com>
2020-08-24 15:41:14 -0500

    customized splash page with user name!!!!

--- src/main/java/org/launchcode/readinglog/controllers/HelloController.java
+++ src/main/java/org/launchcode/readinglog/controllers/HelloController.java
@@ -1,23 +1,35 @@
 package org.launchcode.readinglog.controllers;
 
+import org.launchcode.readinglog.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HelloController {
 
-//    @Autowired
-//    AuthenticationController authenticationController;
-    //HttpServletRequest request;
+    @Autowired
+    AuthenticationController authenticationController;
+//    HttpServletRequest request;
 //    HttpSession session = request.getSession();
 //    User user = authenticationController.getUserFromSession(session);
 
     @RequestMapping(value = "")
     @ResponseBody
     public String index() {
-        //return "Hello" + request.getRemoteUser() + ".  Welcome to the plaintext reading log.  More features coming soon.";
-        return "Hello" + " World" + ".  Welcome to the plaintext reading log.  More features coming soon.";
+
+        //System.out.println(user.getUsername());
+        HttpSession currentSession = (HttpSession) RequestContextHolder.currentRequestAttributes().getSessionMutex();
+        String sessionID = RequestContextHolder.currentRequestAttributes().getSessionId();
+        final User currentUser = authenticationController.getUserFromSession(currentSession);
+
+
+        return "Hello " + currentUser.getUsername() + "!  Welcome to the plaintext reading log.  More features coming soon.";
+        //return "Hello" + " World" + ".  Welcome to the plaintext reading log.  More features coming soon.";
     }
 
     @RequestMapping(value = "goodbye")
