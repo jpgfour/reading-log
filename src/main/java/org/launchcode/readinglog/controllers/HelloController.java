2b26d38219712f2a03e828386018413f516ffa5d
jpgfour <jeremy.grise@gmail.com>
2020-08-24 12:09:33 -0500

    trying to customize splash page with user name

--- src/main/java/org/launchcode/readinglog/controllers/HelloController.java
+++ src/main/java/org/launchcode/readinglog/controllers/HelloController.java
@@ -7,10 +7,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
+//    @Autowired
+//    AuthenticationController authenticationController;
+//    HttpServletRequest request;
+//    HttpSession session = request.getSession();
+//    User user = authenticationController.getUserFromSession(session);
+
     @RequestMapping(value = "")
     @ResponseBody
     public String index() {
-        return "Hello World.  Welcome to the plaintext reading log.  More features coming soon.";
+        //return "Hello" + user.getUsername() + ".  Welcome to the plaintext reading log.  More features coming soon.";
+        return "Hello" + " World" + ".  Welcome to the plaintext reading log.  More features coming soon.";
     }
 
     @RequestMapping(value = "goodbye")
