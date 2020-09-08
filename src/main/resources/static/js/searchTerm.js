30a0fbd211d8f16ab2d993a13179b2846dc8a865
jpgfour <jeremy.grise@gmail.com>
2020-09-08 15:25:14 -0500

    GOT BASIC SEARCH RETURNING RESULTS!!!!!

--- src/main/resources/static/js/searchTerm.js
+++ src/main/resources/static/js/searchTerm.js
@@ -2,6 +2,10 @@
 function returnURL(search) {
 
     var url = "https://www.googleapis.com/books/v1/volumes?q=" + search + "&callback=handleResponse";
+
+    alert("here's the search term: " + search);
+
     return url;
+
 
 }
