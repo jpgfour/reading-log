d6459ab8f33e6ea7796debb6fba2f142793076f4
jpgfour <jeremy.grise@gmail.com>
2020-09-10 11:28:48 -0500

    STILL need to figure out how to load "log" screen - template/controller? that uses google data again? got google data page to load, need to send it to log template instead
    changed maxResults.  added "Harry Potter" as default search term

--- src/main/java/org/launchcode/readinglog/controllers/LogController.java
+++ src/main/java/org/launchcode/readinglog/controllers/LogController.java
@@ -2,6 +2,7 @@ package org.launchcode.readinglog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,11 +11,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("logs")
 public class LogController {
 
-    @RequestMapping("add")
-    public String log(Model model) {
-        //model.addAttribute();
+//    private void getVolumeDataForLogging(String volumeId) {
+//
+//        final String uri = "https://www.googleapis.com/books/v1/volumes/" + volumeId;
+//        RestTemplate restTemplate = new RestTemplate();
+//        Volume result = restTemplate.getForObject(uri,Volume.class);
+//        System.out.println(result);
+//
+//    }
+
+
+
+    @RequestMapping("add/{volumeId}")
+    public String log(Model model, @PathVariable String volumeId) {
+//        TODO: query API based on ID, pull data points, pass data points to Add template
+
+//        getVolumeDataForLogging(volumeId);
+
+        model.addAttribute("volumeId", volumeId);
         return "add";
     }
+
 
     @PostMapping("display")
     public String log(Model model, @RequestParam int userID){
