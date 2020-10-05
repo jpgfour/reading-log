29150acf85fa5c45f1dc5643ce097864f954131c
jpgfour <jeremy.grise@gmail.com>
2020-10-05 16:16:08 -0500

    successfully getting info back from java/jackson query

--- src/main/java/org/launchcode/readinglog/controllers/LogController.java
+++ src/main/java/org/launchcode/readinglog/controllers/LogController.java
@@ -3,12 +3,18 @@ package org.launchcode.readinglog.controllers;
 import org.launchcode.readinglog.models.Log;
 import org.launchcode.readinglog.models.data.LogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Map;
+
 
 @Controller
 @RequestMapping("logs")
@@ -41,11 +47,29 @@ public class LogController {
 
     @PostMapping("add/{volumeId}")
     public String processAddLogForm(@ModelAttribute @Valid Log newLog, Errors errors, Model model, @RequestParam String volumeGoogleId, @RequestParam String volumeName, @RequestParam String date,
-                                    @RequestParam Integer minutesLogged, @RequestParam int pagesLogged, @PathVariable String volumeId) {
+                                    @RequestParam Integer minutesLogged, @RequestParam int pagesLogged, @PathVariable String volumeId) throws IOException {
+
+        //begin code block get google books response using volumeId from Path
+
+        String volumeURL = "https://www.googleapis.com/books/v1/volumes/" + volumeId;
+        RestTemplate restTemplate = new RestTemplate();
+        String volumeResponse = restTemplate.getForObject(volumeURL, String.class);
+
+        JsonParser springParser = JsonParserFactory.getJsonParser();
+        Map<String, Object> map = springParser.parseMap(volumeResponse);
+
+        String mapArray[] = new String[map.size()];
+        System.out.println("Items found: " + mapArray.length);
+
+        int i = 0;
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+            i++;
+        }
+
+        //end code block get google books response
 
         newLog.setVolumeGoogleId(volumeId);
-
-
 
         logRepository.save(newLog);
 
